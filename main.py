ca55c96868b2ae19fedfbe8b3c8964634bd6671a
asenetcky <me@asenetcky.dev>
2025-06-03 18:24:43 +0000

    feat: add mvp

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 
-# import polars as pl
-# import sodapy
+import polars as pl
+from sodapy import Socrata
 
 APP_TOKEN = os.environ["APP_TOKEN"]
 
 
 def main():
-    print("Hello from mvp!")
+    with Socrata("data.ct.gov", APP_TOKEN) as client:
+        results = client.get("qhtt-czu2", limit=10, offset=10)
+        lf = pl.LazyFrame(results)
+        print(lf.collect())
 
 
 if __name__ == "__main__":
